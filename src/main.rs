7fd33c7137f65f839d94ff735d136476f4fc6135
chris <chris@ngmi.ai>
2022-04-05 16:59:59 +0200

    simple natural language detection using lingua

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use lingua::Language::{English, French, German, Spanish};
+use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
+use std::io;
+
+fn main() {
+    println!("Enter Text you want the language detected for, can detect En, Fr, Ger, Spa: ");
+    let mut lan = String::new();
+    io::stdin()
+        .read_line(&mut lan)
+        .expect("Failed to read line");
+
+    let languages = vec![English, French, German, Spanish];
+    let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
+    let detected_language: Option<Language> = detector.detect_language_of(&lan);
+
+    println!("Detected language: {:?}", detected_language);
+}
